fed354e93a3b321fcdff5fe439694f09120cc0b6
Tomer Berenstein <tomerbrotem@gmail.com>
2024-01-30 22:12:29 +0200

    Added the FaslePostivie Class refactored includes and the Bloom Filter class to add the usage

--- include/BloomFilter.hpp
+++ include/BloomFilter.hpp
@@ -3,10 +3,12 @@
 
 #include <vector>
 #include <string>
+#include <unordered_set>
 
 class BloomFilter
 {
 private:
+    std::unordered_set<std::string> falsePositive; // Control set for falsepositive checkings
     std::vector<bool> bitArray;                    // The bit array for the filter.
     std::vector<std::string> hashFunctionIDs;      // Stores the hash function identifiers
 
@@ -17,6 +19,7 @@ public:
     const std::vector<std::string> &getHashFunctions();             // Returns hash function IDs.
     void setBitArray(int index);                                    // Sets a bit in the array.
     int getBitArraySize();                                          // Returns the size of the bit array.
+    std::unordered_set<std::string> getFalsePositives();            // Gets the control set.
     void insertFaslePositives(std::string &url);                    // Adds a URL to the control set.
 };
 
