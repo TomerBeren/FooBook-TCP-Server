0be984dc1d51e3abc60f6ec0f558938a0b7a9dff
Roei <RoeiM442@gmail.com>
2024-01-30 23:08:01 +0200

    Added tests for the command init.

---
+++ tests/CommandInitTest.cpp
@@ -0,0 +1,37 @@
+#include "CommandInitializer.hpp"
+#include "CommandFactory.hpp"
+#include "AddUrlCommand.hpp" 
+#include <gtest/gtest.h>
+#include "IMenu.hpp"
+#include "App.hpp"
+#include "ConsoleMenu.hpp"
+
+class CommandInitializerTest : public ::testing::Test
+{
+protected:
+    CommandInitializer initializer;
+
+    // Ensure that the self-registration of AddUrlCommand occurs
+    static void SetUpTestSuite()
+    {
+        // Trigger the static registration of AddUrlCommand
+        (void)AddUrlCommand::registered;
+    }
+};
+
+TEST_F(CommandInitializerTest, InitializationTest)
+{
+    auto &commands = initializer.getInitializedCommands();
+    // Check if the commands map is not empty
+    ASSERT_FALSE(commands.empty());
+}
+
+TEST_F(CommandInitializerTest, CommandsMapContentTest)
+{
+    auto &commands = initializer.getInitializedCommands();
+    // Check if the "1" command is present in the commands map
+    ASSERT_NE(commands.find("1"), commands.end());
+    // Check if the command is of the correct type
+    auto *cmd = commands["1"];
+    ASSERT_TRUE(dynamic_cast<AddUrlCommand *>(cmd) != nullptr);
+}
